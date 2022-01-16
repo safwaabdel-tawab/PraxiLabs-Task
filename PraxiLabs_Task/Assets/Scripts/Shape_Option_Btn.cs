cd3b912b21140d8d861a0a4e601969f0a2533875
Safwa <safwa.tawab@gmail.com>
2022-01-17 00:47:14 +0200

    Viewing objects when clicked on them

---
+++ PraxiLabs_Task/Assets/Scripts/Shape_Option_Btn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Shape_Option_Btn : MonoBehaviour
+{
+    [SerializeField] StringField object_SF;
+    [SerializeField] Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(delegate
+        {
+            Manager.Instance.ViewObject(object_SF.Value);
+        });
+    }
+}
