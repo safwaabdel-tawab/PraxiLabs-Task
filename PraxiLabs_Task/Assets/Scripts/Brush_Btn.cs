7bb42029f6e385da08e175885c4d50d089f0412e
Safwa <safwa.tawab@gmail.com>
2022-01-16 21:37:12 +0200

    Creating Brush_Btn script also making IColor interface and used it in Brush_Btn script

---
+++ PraxiLabs_Task/Assets/Scripts/Brush_Btn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface IColor
+{
+    Color MyColor();
+}
+public class Brush_Btn : MonoBehaviour, IColor
+{
+    [SerializeField] Image brush_Img;
+    [SerializeField] Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(delegate
+        {
+            Manager.Instance.ColorSelected(MyColor());
+        });
+    }
+    public Color MyColor()
+    {
+        return brush_Img.color;
+    }
+}
