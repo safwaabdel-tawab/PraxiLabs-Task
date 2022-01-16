5d8bd0fcefd1c9f357f8e415376028d7120efae6
Safwa <safwa.tawab@gmail.com>
2022-01-16 20:46:55 +0200

    Making Object_Controller and Manager scripts

---
+++ PraxiLabs_Task/Assets/Scripts/Manager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+
+public class Manager : MonoBehaviour
+{
+    private static Manager _instance;
+    public static Manager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<Manager>();
+            return _instance;
+        }
+    }
+
+    [SerializeField] ObjectDataList_SO objectsData_List;
+    [SerializeField] Transform viewPoint;
+    [SerializeField, ReadOnly] Color currentColorInUse;
+
+    public Dictionary<GameObject, ObjectData_SO> InstanObjects_Dict;
+    public int IndexOfObjectViewed { get; set; }
+
+    public delegate void ColorChangedEvent(Color _color);
+    public event ColorChangedEvent OnColorChanged_Event;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        InstanObjects_Dict = new Dictionary<GameObject, ObjectData_SO>();
+
+        foreach (ObjectData_SO _object in objectsData_List.list)
+        {
+            GameObject objectInstan = Instantiate(_object.objectPrefab, viewPoint);
+
+            Object_Controler object_Controler = objectInstan.GetComponent<Object_Controler>();
+            object_Controler = new Object_Controler();
+
+            objectInstan.gameObject.SetActive(false);
+
+            InstanObjects_Dict.Add(objectInstan, _object);
+        }
+    }
+
+    public void ViewObject(string _name)
+    {
+        KeyValuePair<GameObject, ObjectData_SO> itemTobeViewed = InstanObjects_Dict.Where(p => p.Value.objectName == _name).First();
+        itemTobeViewed.Key.SetActive(true);
+    }
+
+    public void ColorSelected(Color _colorToBe)
+    {
+        if (ColorsDoMatch(currentColorInUse, _colorToBe)) return;
+
+        currentColorInUse = _colorToBe;
+
+        OnColorChanged_Event(currentColorInUse);
+    }
+
+    public bool ColorsDoMatch(Color _color1, Color _color2)
+    {
+        return _color1.r == _color2.r && _color1.g == _color2.g && _color1.b == _color2.b && _color1.a == _color2.a;
+    }
+}
---
+++ PraxiLabs_Task/Assets/Scripts/Object_Controler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Controler : MonoBehaviour
+{
+    [SerializeField] 
+    private ObjectData_SO objectData_SO;
+
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+    public Object_Model model { get; private set; }
+
+    public Object_Controler()
+    {
+        model = new Object_Model();
+        _renderer = GetComponent<Renderer>();
+
+        Manager.Instance.OnColorChanged_Event += ChangeColor;
+    }
+
+    public void ChangeColor(Color color)
+    {
+        _renderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetColor("_Color", color);
+        _renderer.SetPropertyBlock(_propBlock);
+    }
+}
