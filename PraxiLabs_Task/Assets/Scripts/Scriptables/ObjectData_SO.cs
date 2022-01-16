dee4822f64610f42d3f51ca6f1cedab064304208
Safwa <safwa.tawab@gmail.com>
2022-01-16 16:32:28 +0200

    Adding ObjectData_SO script

---
+++ PraxiLabs_Task/Assets/Scripts/Scriptables/ObjectData_SO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/ObjectData")]
+public class ObjectData_SO : ScriptableObject
+{
+    public string objectName;
+    public GameObject objectPrefab;
+}
