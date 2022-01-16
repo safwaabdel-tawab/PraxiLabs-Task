ae87dfbaa1095de7d446c12f6229fc3c4739f5c3
Safwa <safwa.tawab@gmail.com>
2022-01-16 16:33:39 +0200

    Adding ObjectDataList_SO script

---
+++ PraxiLabs_Task/Assets/Scripts/Scriptables/ObjectDataList_SO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/List/ObjectDataList_SO")]
+public class ObjectDataList_SO : ScriptableObject
+{
+    public List<ObjectData_SO> list;
+   
+}
