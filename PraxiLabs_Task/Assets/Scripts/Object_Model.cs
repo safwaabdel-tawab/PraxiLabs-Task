05eceffb7eca9e0696f3d9b05eafd65d24fcee09
Safwa <safwa.tawab@gmail.com>
2022-01-16 20:45:53 +0200

    Creating Object_Model script

---
+++ PraxiLabs_Task/Assets/Scripts/Object_Model.cs
@@ -0,0 +1,18 @@
+﻿[System.Serializable]
+public class Object_Model
+{
+    public float[] Rotation;
+    public float[] CurrentColor;
+
+    public Object_Model()
+    {
+        Rotation = new float[] { };
+        CurrentColor = new float[] { };
+    }
+
+    public Object_Model(float[] rotation, float[] currentColor)
+    {
+        Rotation = rotation;
+        CurrentColor = currentColor;
+    }
+}
